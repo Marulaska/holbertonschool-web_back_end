ca820e6057b6940f6ee79bf9871799cf180d0d25
Mariana Echeto <echetomariana@gmail.com>
2024-01-05 15:05:25 -0300

    0-add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Args:
+        a: (float)
+        b: (float)
+
+    Returns:
+        a+b:float
+    """
+    return a + b
