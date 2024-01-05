23984cf521f5a9e88b9f54594ec0ad35e2a416cb
Mariana Echeto <echetomariana@gmail.com>
2024-01-05 15:23:47 -0300

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+type-annotations Concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Args:
+        first: str
+        second: str
+
+    Returns:
+        str
+    """
+    return str1 + str2
