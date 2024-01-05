a48c42d7f1b3d20685c7e22c2bbe4dca1d421e26
Mariana Echeto <echetomariana@gmail.com>
2024-01-05 17:51:03 -0300

    to_str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+type-annotations to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        n (Any): value
+
+    Returns:
+        str: string
+    """
+    return str(n)
