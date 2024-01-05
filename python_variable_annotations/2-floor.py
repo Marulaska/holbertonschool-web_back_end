ab0be33bbda91ddbd19e1307f472d2b0a57d2ae9
Mariana Echeto <echetomariana@gmail.com>
2024-01-05 16:31:40 -0300

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+type-annotations Concat
+"""
+from math import floor
+
+
+def floor_function(n: float) -> int:
+    """
+    Args:
+        n (float): float number
+
+    Returns:
+        int: int number
+    """
+    r: int = floor(n)
+    return r
