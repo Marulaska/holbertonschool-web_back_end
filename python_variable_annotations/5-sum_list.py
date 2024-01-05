78880ec05bf90ed41729b31fc8c10370f799b3a0
Mariana Echeto <echetomariana@gmail.com>
2024-01-05 18:23:44 -0300

    sum_list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+type-annotations Floor
+"""
+from typing import List
+
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+
+    Args:
+        input_list: List of float
+
+    Returns:
+        float
+    """
+    return sum(input_list)
