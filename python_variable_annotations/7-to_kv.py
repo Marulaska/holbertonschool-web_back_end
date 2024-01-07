0341d325a6d909dea84ad197980a3bd7e97a28df
Mariana Echeto <echetomariana@gmail.com>
2024-01-06 21:33:22 -0300

    to kv

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -15,5 +15,5 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     Returns:
         Dict:[str, Union[float]]
     """
-    r: Tuple[str, float] = {k: float(v**2)}
+    r: Tuple[str, float] = (k, float(v**2))
     return r
