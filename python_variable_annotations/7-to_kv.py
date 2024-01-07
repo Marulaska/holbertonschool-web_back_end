8e2c1cde7f731082135dc0ac1a07be3f5be3a1f7
Mariana Echeto <echetomariana@gmail.com>
2024-01-06 21:15:01 -0300

    to kv

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+type-annotations to kv
+"""
+from typing import Any, Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[float]]:
+    """
+
+    Args:
+        k: (str)
+        v: (Union[int, float])
+
+    Returns:
+        Dict:[str, Union[float]]
+    """
+    r: Dict[str, Union[int, float]] = {k: v**2}
+    return r
