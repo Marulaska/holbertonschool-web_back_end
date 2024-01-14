330646762064e7f8d3e2bf5319f912ca622f170e
Mariana Echeto <echetomariana@gmail.com>
2024-01-14 16:31:07 -0300

    logs stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+provides some stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    print(str(logs.count_documents({})) + " logs")
+    print("\tmethod GET: " + str(logs.count_documents({"method" : "GET"})))
+    print("\tmethod POST: " + str(logs.count_documents({"method" : "POST"})))
+    print("\tmethod PUT: " + str(logs.count_documents({"method" : "PUT"})))
+    print("\tmethod PATCH: " + str(logs.count_documents({"method" : "PATCH"})))
+    print("\tmethod DELETE: " + str(logs.count_documents({"method" : "DELETE"})))
+    print(str(logs.count_documents({"method" : "GET","path" : "/status"})) + " status check")
