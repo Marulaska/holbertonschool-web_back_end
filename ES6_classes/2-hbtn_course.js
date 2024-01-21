26373cdf99dc9ae005244bbc6ee333736190a308
Mariana Echeto <echetomariana@gmail.com>
2024-01-21 19:10:03 -0300

    fixerror

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,13 +1,13 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-      throw new Error('Name must be a string');
+      throw TypeError('Name must be a string');
     }
     if (typeof length !== 'number') {
-      throw new Error('Length must be a number');
+      throw TypeError('Length must be a number');
     }
     if (!Array.isArray(students)) {
-      throw new Error('Students must be an array of strings');
+      throw TypeError('Students must be an array of strings');
     }
     this._name = name;
     this._length = length;
@@ -28,21 +28,21 @@ export default class HolbertonCourse {
 
   set name(name) {
     if (typeof name !== 'string') {
-      throw new Error('Name must be a string');
+      throw TypeError('Name must be a string');
     }
     this._name = name;
   }
 
   set length(length) {
     if (typeof length !== 'number') {
-      throw new Error('Length must be a number');
+      throw TypeError('Length must be a number');
     }
     this._length = length;
   }
 
   set students(students) {
     if (!Array.isArray(students)) {
-      throw new Error('Students must be an array of strings');
+      throw TypeError('Students must be an array of strings');
     }
     this._students = students;
   }
