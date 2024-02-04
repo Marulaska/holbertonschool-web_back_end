ffd3b654bd401200bebee5d2934f8b683401e52a
Mariana Echeto <echetomariana@gmail.com>
2024-02-04 03:23:45 -0300

    substring

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  let retString = '';
+  if (startString.length > 0) {
+    set.forEach((value) => {
+      // console.log(value.substring(0,startString.length))
+      if (value.substring(0, startString.length) === startString) {
+        retString += `${value.substring(startString.length)}-`;
+      }
+    });
+  }
+  return retString.substring(0, retString.length - 1);
+}
